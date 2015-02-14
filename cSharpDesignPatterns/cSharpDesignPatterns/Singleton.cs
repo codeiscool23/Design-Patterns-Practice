b94f9a67a46631acb5817725fc72f05857a97043
codeiscool23 <benjamin.schaefer@gmail.com>
2015-02-14 18:04:26 -0500

    this is to have accessibility to commonly used design patterns

---
+++ cSharpDesignPatterns/cSharpDesignPatterns/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDesignPatterns
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance = null;
+        private static readonly object Padlock = new object();
+
+        Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (Padlock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
